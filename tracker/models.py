5a100aa33bc8e1b11eb53d051bbc44da584594a8
Md Ibrahim Hossain <outsideworkibrahim@gmail.com>
2024-04-03 22:38:49 +0600

    Add create and show api end point for Device, Employee and transaction model

--- tracker/models.py
+++ tracker/models.py
@@ -19,9 +19,9 @@ class Company(models.Model):
     def __str__(self):
         return self.name
 
-
 class Employee(models.Model):
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    username = models.CharField(max_length=30)
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     email = models.EmailField()
@@ -38,7 +38,7 @@ class Transaction(models.Model):
     employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
     device = models.ForeignKey(Device, on_delete=models.CASCADE)
     checked_out = models.DateTimeField()
-    checked_in = models.DateTimeField(null=True, blank=True)
+    checked_in = models.DateTimeField()
     condition_on_checkout = models.CharField(max_length=100)
     condition_on_checkin = models.CharField(max_length=100, null=True, blank=True)
 
--- tracker/urls.py
+++ tracker/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path 
-from .views import CreateCompanyAPI, ShowCompanyAPI
+from . import views
 
 urlpatterns = [
-  path('create_company/', CreateCompanyAPI.as_view(), name='create-company'),
-  path('show_company/', ShowCompanyAPI.as_view(), name='show-company')
+  path('create_company/', views.CreateCompanyAPI.as_view(), name='create-company'),
+  path('show_company/', views.ShowCompanyAPI.as_view(), name='show-company'),
+  path('create_employee/', views.CreateEmployeeAPI.as_view(), name='create-employee'),
+  path('show_employee/', views.ShowEmployeeAPI.as_view(), name='show-employee'),
+  path('create_device/', views.CreateDeviceAPI.as_view(), name='create-device'),
+  path('show_device/', views.ShowDeviceAPI.as_view(), name='show-device'),
+  path('create_transaction/', views.CreateTransactionAPI .as_view(), name='create-transaction'),
+  path('show_transaction/', views.ShowTransactionAPI.as_view(), name='show-transaction'),
 ]
