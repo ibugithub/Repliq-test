c4846638af15c8de3c35a48d794860451dae5b0d
Md Ibrahim Hossain <outsideworkibrahim@gmail.com>
2024-04-03 22:58:30 +0600

    Add Automated API Documentation

--- tracker/urls.py
+++ tracker/urls.py
@@ -1,5 +1,19 @@
 from django.urls import path 
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
 from . import views
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Device Tracker API",
+        default_version='v1',
+        description="API documentation for deviceTracker project",
+        contact=openapi.Contact(email="outsideworkibrahim@gmail.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+)
 
 urlpatterns = [
   path('create_company/', views.CreateCompanyAPI.as_view(), name='create-company'),
@@ -10,4 +24,6 @@ urlpatterns = [
   path('show_device/', views.ShowDeviceAPI.as_view(), name='show-device'),
   path('create_transaction/', views.CreateTransactionAPI .as_view(), name='create-transaction'),
   path('show_transaction/', views.ShowTransactionAPI.as_view(), name='show-transaction'),
+  path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+  path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
